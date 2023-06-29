a969fa09947fedcdde52cc9f289f5d29a9ceabed
dosymep <dosymep@gmail.com>
2023-06-29 16:55:01 +0300

    Заменил скрипты обновить и настройки

--- 2D.tab/Установки.panel/настройки.stack/Настройки.smartbutton/script.py
+++ 2D.tab/Установки.panel/настройки.stack/Настройки.smartbutton/script.py
@@ -1,47 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import clr
-clr.AddReference('dosymep.Revit.dll')
-clr.AddReference('dosymep.Bim4Everyone.dll')
+from pyrevit.loader.sessionmgr import execute_command
+execute_command("01dotbim-bim-установки-настройки-настройки")
 
-from System import InvalidOperationException, OperationCanceledException
-
-from Autodesk.Revit.DB import *
-from Autodesk.Revit.UI import *
-from Autodesk.Revit.ApplicationServices import LanguageType
-
-from pyrevit import HOST_APP
-from pyrevit import EXEC_PARAMS
-from pyrevit.userconfig import user_config
-
-from dosymep.Revit import *
-from dosymep.Bim4Everyone import *
-
-from dosymep_libs.bim4everyone import *
-
-def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
-    user_config.auto_update = True
-    user_config.check_updates = True
-    user_config.save_changes()
-
-    if HOST_APP.language == LanguageType.Russian:
-        user_config.user_locale = 'ru'
-
-    if HOST_APP.language == LanguageType.English_USA:
-        user_config.user_locale = 'en_us'
-
-
-@log_plugin(EXEC_PARAMS.command_name)
-def script_execute(plugin_logger):
-    try:
-        invoke_command(PlatformCommandIds.PlatformSettingsCommandId)
-
-        user_config.reload()
-        from pyrevit.loader.sessionmgr import execute_command
-        execute_command("01dotbim-bim-установки-настройки-обновить")
-    except OperationCanceledException:
-        show_canceled_script_notification()
-
-
-if __name__ == '__main__':
-    script_execute()
--- 2D.tab/Установки.panel/настройки.stack/Обновить.pushbutton/script.py
+++ 2D.tab/Установки.panel/настройки.stack/Обновить.pushbutton/script.py
@@ -1,30 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from pyrevit import EXEC_PARAMS
-from pyrevit import script
-from pyrevit import forms
-from pyrevit.loader import sessionmgr
-from pyrevit.loader import sessioninfo
-
-from dosymep_libs.bim4everyone import *
-
-__cleanengine__ = True
-
-@notification()
-@log_plugin(EXEC_PARAMS.command_name)
-def script_execute(plugin_logger):
-    if EXEC_PARAMS.executed_from_ui:
-        forms.alert('Вы уверены что хотите обновить?',
-                          yes=True, no=True, exitscript=True)
-
-    logger = script.get_logger()
-    results = script.get_results()
-
-    # re-load pyrevit session.
-    logger.info('Reloading....')
-    sessionmgr.load_session()
-
-    results.newsession = sessioninfo.get_session_uuid()
-
-
-script_execute()
+from pyrevit.loader.sessionmgr import execute_command
+execute_command("01dotbim-bim-установки-настройки-обновить")
