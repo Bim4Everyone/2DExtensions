34ac33f7584f3dcedbe5dfa639b988431c429367
RamazanovSham <ltlmlfs@gmail.com>
2023-11-23 13:42:29 +0300

    Создать жук по плану этажа: Добавлена поддержка связанных файлов (#2)
    Добавлена поддержка связанных файлов
    
    ---------
    
    Co-authored-by: dosymep <dosymep@gmail.com>

--- 2D.tab/Листы.panel/Листы 2.stack/Другие команды.pulldown/Создать жук по плану этажа.pushbutton/script.py
+++ 2D.tab/Листы.panel/Листы 2.stack/Другие команды.pulldown/Создать жук по плану этажа.pushbutton/script.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import clr
+
 clr.AddReference('System')
 clr.AddReference("System.Windows.Forms")
 
@@ -12,6 +13,7 @@ clr.AddReference("dosymep.Revit.dll")
 clr.AddReference("dosymep.Bim4Everyone.dll")
 
 import dosymep
+
 clr.ImportExtensions(dosymep.Revit)
 clr.ImportExtensions(dosymep.Bim4Everyone)
 
@@ -29,9 +31,22 @@ from pyrevit.forms import Reactive, reactive
 
 from dosymep_libs.bim4everyone import *
 
-
 doc = __revit__.ActiveUIDocument.Document
 view = doc.ActiveView
+
+
+class ElementWall:
+    def __init__(self, wall=None, trans=None):
+        self.wall = wall
+        self.trans = trans
+
+    def __eq__(self, other):
+        return (self.wall.Id == other.wall.Id
+                and self.trans.Origin.IsAlmostEqualTo(other.trans.Origin))
+
+    def __hash__(self):
+        # Нужно не точное сравнение для XYZ
+        return 0  # hash((self.wall.Id, self.trans.Origin))
 
 
 class MainWindow(forms.WPFWindow):
@@ -58,7 +73,6 @@ class MainWindowViewModel(Reactive):
         self.__walls = walls
         self.__base_legend = base_legend
         self.__legend_names = legend_names
-
         self.__create_legend_command = CreateLegendCommand(self)
 
     @reactive
@@ -119,7 +133,6 @@ class CreateLegendCommand(ICommand):
 
     def __init__(self, view_model, *args):
         ICommand.__init__(self, *args)
-
         self.__view_model = view_model
         self.__view_model.PropertyChanged += self.ViewModel_PropertyChanged
 
@@ -168,7 +181,6 @@ class CreateLegendCommand(ICommand):
         walls = self.__view_model.walls
         legend_name = self.__view_model.legend_name
         legend_scale = self.__view_model.legend_scale
-
         base_legend = self.__view_model.base_legend
         legend_names = self.__view_model.legend_names
 
@@ -178,20 +190,21 @@ class CreateLegendCommand(ICommand):
         transform = Transform.CreateTranslation(XYZ.Zero)
         transform = transform.ScaleBasis(scale)
 
+        legend = view
         with revit.Transaction("BIM: Создание жука по плану этажа"):
             legend_id = base_legend.Duplicate(ViewDuplicateOption.Duplicate)
             legend = doc.GetElement(legend_id)
-
             legend.Name = legend_name
             legend.SetParamValue(BuiltInParameter.VIEW_SCALE_PULLDOWN_METRIC, int(legend_scale))
-
             for wall in walls:
-                if not '(В)' in wall.Name:
-                    curve = wall.Location.Curve
-                    scaled_curve = curve.CreateTransformed(transform)
+                if '(В)' not in wall.wall.Name:
+                    curve = wall.wall.Location.Curve
+                    scaled_curve = curve.CreateTransformed(transform.Multiply(wall.trans))
+                    doc.Create.NewDetailCurve(legend, scaled_curve)
 
-                    if scaled_curve:
-                        doc.Create.NewDetailCurve(legend, scaled_curve)
+
+        UIDocument(doc).ActiveView = legend
+
 
     @staticmethod
     def __is_int(value):
@@ -202,22 +215,154 @@ class CreateLegendCommand(ICommand):
             return False
 
 
+class ExportWall2D(IExportContext2D):
+    def __init__(self, document, link_instances):
+        self.__document = document
+        self.__walls_list = set()
+        self.__link_instances = link_instances
+
+    def OnElementBegin2D(self, node):
+        element = node.Document.GetElement(node.ElementId)
+        if not element.InAnyCategory(BuiltInCategory.OST_Walls):
+            return RenderNodeAction.Skip
+
+        if node.LinkInstanceId.IsNull():
+            self.__walls_list.add(ElementWall(wall=element, trans=Transform.Identity))
+            return RenderNodeAction.Proceed
+
+        link_instance = next((link_instance for link_instance in self.__link_instances
+                              if link_instance.Id == node.LinkInstanceId))
+
+        transform = link_instance.GetTotalTransform() if link_instance else Transform.Identity
+        self.__walls_list.add(ElementWall(wall=element, trans=transform))
+
+        return RenderNodeAction.Proceed
+
+    def get_element_list(self):
+        return self.__walls_list
+
+
+    def OnCurve(self, node):
+        return RenderNodeAction.Skip
+
+
+    def OnElementBegin(self, element_id):
+        return RenderNodeAction.Skip
+
+
+    def OnElementEnd(self, element_id):
+        pass
+
+
+    def OnFaceBegin(self, node):
+        return RenderNodeAction.Skip
+
+
+    def OnFaceEnd(self, node):
+        pass
+
+
+    def OnInstanceBegin(self, node):
+        return RenderNodeAction.Skip
+
+
+    def OnInstanceEnd(self, node):
+        pass
+
+
+    def OnLinkBegin(self, node):
+        return RenderNodeAction.Proceed
+
+
+    def OnLinkEnd(self, node):
+        pass
+
+
+    def OnLight(self, node):
+        pass
+
+
+    def OnMaterial(self, node):
+        pass
+
+
+    def OnPolymesh(self, node):
+        pass
+
+
+    def OnRPC(self, node):
+        pass
+
+
+    def OnElementEnd2D(self, node):
+        return None
+
+
+    def OnFaceEdge2D(self, node):
+        return RenderNodeAction.Skip
+
+
+    def OnFaceSilhouette2D(self, node):
+        return RenderNodeAction.Skip
+
+
+    def OnLineSegment(self, segment):
+        return None
+
+
+    def OnPolyline(self, node):
+        return RenderNodeAction.Skip
+
+
+    def OnPolylineSegments(self, segments):
+        return None
+
+
+    def OnText(self):
+        return None
+
+
+    def Finish(self):
+        pass
+
+
+    def Start(self):
+        return True
+
+
+    def IsCanceled(self):
+        return False
+
+
+    def OnViewBegin(self, node):
+        return RenderNodeAction.Proceed
+
+
+    def OnViewEnd(self, element_id):
+        pass
+
+
 @notification()
 @log_plugin(EXEC_PARAMS.command_name)
 def script_execute(plugin_logger):
     if view.ViewType != ViewType.FloorPlan:
         forms.alert("Активный вид должен быть планом этажа.", exitscript=True)
 
-    walls = [x for x in
-             FilteredElementCollector(doc, view.Id).OfClass(Wall).WhereElementIsNotElementType().ToElements()]
+    link_instances = list(FilteredElementCollector(doc)
+                          .OfCategory(BuiltInCategory.OST_RvtLinks)
+                          .ToElements())
+
+    export_wall = ExportWall2D(doc, link_instances)
+    exporter = CustomExporter(doc, export_wall)
+    exporter.Export(view)
+
+    walls = export_wall.get_element_list()
     if not walls:
         forms.alert("На активном виде нет стен.", exitscript=True)
 
     legends = (x for x in FilteredElementCollector(doc).OfClass(View) if x.ViewType == ViewType.Legend)
-
     legends_names = (x.Name for x in legends)
     legends = (x for x in legends if x.CanViewBeDuplicated(ViewDuplicateOption.Duplicate))
-
     base_legend = next(legends, None)
     if not base_legend:
         forms.alert("В модели нет легенды для копирования.", exitscript=True)
