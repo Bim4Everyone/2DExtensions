da1a81ceaa84835d1f5e2aac1e4620e2d3ce8d5b
Vladimir Stroganov <vlastroganov@gmail.com>
2024-12-19 12:42:06 +0300

    Цветовые области: Обновлены скрипты по их вырезанию и объединению (#15)

---
+++ 2D.tab/Оформление.panel/Оформление 2.stack/Вырезать цветовые области.pushbutton/script.py
@@ -0,0 +1,73 @@
+# coding=utf-8
+import Autodesk.Revit.Exceptions
+import clr
+
+clr.AddReference("dosymep.Revit.dll")
+clr.AddReference("dosymep.Bim4Everyone.dll")
+
+import dosymep
+
+clr.ImportExtensions(dosymep.Revit)
+clr.ImportExtensions(dosymep.Bim4Everyone)
+
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.UI.Selection import ISelectionFilter
+from Autodesk.Revit.UI.Selection import ObjectType
+
+from pyrevit import forms
+from pyrevit import revit
+from pyrevit import script
+from pyrevit import HOST_APP
+from pyrevit import EXEC_PARAMS
+
+
+from dosymep_libs.bim4everyone import *
+from dosymep.Revit.Geometry import *
+
+import regions
+
+
+class RevitRepository:
+    def __init__(self):
+        self.__doc = HOST_APP.doc # type: Document
+        self.__uidoc = HOST_APP.uidoc
+
+    def get_loops(self, main_region, cutting_regions):
+        main_solid, cutting_solids = regions.get_solids(main_region, cutting_regions)
+        result_solids = self.cut_solids(main_solid, cutting_solids)
+        result_solids = regions.unite_solids(result_solids)
+        return regions.get_bottom_loops(result_solids, regions.get_normal(main_region))
+
+    @staticmethod
+    def cut_solids(first_solid, second_solids):
+        result = first_solid
+        for solid in second_solids:
+            result = BooleanOperationsUtils.ExecuteBooleanOperation(
+                result,
+                solid,
+                BooleanOperationsType.Difference)
+        return regions.unite_solids([result])
+
+
+@notification()
+@log_plugin(EXEC_PARAMS.command_name)
+def script_execute(plugin_logger):
+    repo = RevitRepository()
+    main_region = regions.pick_region("Выберите основную цветовую область") # type: FilledRegion
+    cutting_regions = regions.pick_regions(main_region, "Выберите цветовые области для вырезания") # type: list
+    if not cutting_regions:
+        raise System.OperationCanceledException()
+    loops = repo.get_loops(main_region, cutting_regions)
+    try:
+        regions.create_region(main_region.GetTypeId(),
+                              main_region.OwnerViewId,
+                              loops,
+                              "BIM: Создание цветовой области")
+    except Autodesk.Revit.Exceptions.ApplicationException:
+        script.output.get_output().close()
+        forms.alert("Нельзя вырезать цветовые области. Скорее всего из-за самопересекающихся контуров.",
+                    exitscript=True)
+    regions.delete_element(main_region.Id, "BIM: Удаление старой цветовой области")
+
+
+script_execute()
---
+++ 2D.tab/Оформление.panel/Оформление 2.stack/Объединить цветовые области.pushbutton/script.py
@@ -0,0 +1,66 @@
+# coding=utf-8
+import Autodesk.Revit.Exceptions
+import clr
+
+clr.AddReference("dosymep.Revit.dll")
+clr.AddReference("dosymep.Bim4Everyone.dll")
+
+import dosymep
+
+clr.ImportExtensions(dosymep.Revit)
+clr.ImportExtensions(dosymep.Bim4Everyone)
+
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.UI.Selection import ISelectionFilter
+from Autodesk.Revit.UI.Selection import ObjectType
+
+from pyrevit import forms
+from pyrevit import revit
+from pyrevit import script
+from pyrevit import HOST_APP
+from pyrevit import EXEC_PARAMS
+
+
+from dosymep_libs.bim4everyone import *
+from dosymep.Revit.Geometry import *
+
+import regions
+
+
+class RevitRepository:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def merge_regions(main_region, other_regions):
+        main_solids, other_solids = regions.get_solids(main_region, other_regions)
+        solids = list(other_solids)
+        solids.append(main_solids)
+        result_solids = regions.unite_solids(solids)
+        return regions.get_bottom_loops(result_solids, regions.get_normal(main_region))
+
+
+@notification()
+@log_plugin(EXEC_PARAMS.command_name)
+def script_execute(plugin_logger):
+    repo = RevitRepository() # type: RevitRepository
+    main_region = regions.pick_region("Выберите основную цветовую область") # type: FilledRegion
+    other_regions = regions.pick_regions(main_region, "Выберите цветовые области для объединения") # type: list
+    if not other_regions:
+        raise System.OperationCanceledException()
+    loops = repo.merge_regions(main_region, other_regions)
+    try:
+        regions.create_region(main_region.GetTypeId(),
+                              main_region.OwnerViewId,
+                              loops,
+                              "BIM: Создание цветовой области")
+    except Autodesk.Revit.Exceptions.ApplicationException:
+        script.output.get_output().close()
+        forms.alert("Нельзя объединить цветовые области. Скорее всего из-за самопересекающихся контуров",
+                    exitscript=True)
+    ids = [i.Id for i in other_regions]
+    ids.append(main_region.Id)
+    regions.delete_elements(ids, "BIM: Удаление старых цветовых областей")
+
+
+script_execute()
